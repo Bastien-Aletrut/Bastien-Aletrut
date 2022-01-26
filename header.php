62398d6e44cbdef0b9ceaaf98d4f5dba430346b3
bastien <aletrut.bastien@gmail.com>
2022-01-26 21:48:09 +0100

    fix 1

---
+++ header.php
@@ -0,0 +1,7 @@
+<nav style="" class="navbar navbar-dark bg-dark shadow-sm fixed-top">
+    <div style="text-align: center; width: 100%">
+        <a class="navbar-brand" style="font-size: 30px" href="index.php">
+            Grand Bazar de Blagnac
+		</a>
+    </div>
+</nav>
