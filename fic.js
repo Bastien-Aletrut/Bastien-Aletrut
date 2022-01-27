7bbbf35a7132e82b28a561414083f6c2ac15a422
bastien <aletrut.bastien@gmail.com>
2022-01-28 00:12:29 +0100

    fix 3

--- fic.js
+++ fic.js
@@ -1,1 +1,16 @@
+$('.btn').tilt({ scale: 1.1, speed: 1000 });
 
+
+window.addEventListener('click', (e) => {
+    console.log(e);
+
+   const rond = document.createElement('div');
+   rond.className = 'clickAnim';
+   rond.style.top = `${e.pageY - 50}px`;
+   rond.style.left = `${e.pageX - 50}px`;
+   document.body.appendChild(rond);
+
+   setTimeout(() => {
+       rond.remove();
+   }, 1500)
+})
